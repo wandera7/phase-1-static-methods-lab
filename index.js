ca1a6a2742331b39bf4d7f5fc787d19351035344
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-18 18:12:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+   return string.replace(/[^a-z0-9 - ' -]/gi, '')
+  }
+  static titleize(string){
+  let arr = [];
+  let exludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+  arr = string.split(' ');
+  return arr.map((word, i) =>
+  {
+     return exludeWords.includes(word) && i!=0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1);
+
+  }).join(' ')
+   }
 }
